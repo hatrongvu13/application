f740bd8e6f02e99e67a7f1f60972d5900277734b
hatrongvu13 <hatrongvu13@gmail.com>
2021-10-04 11:04:14 +0700

    add check external ip

---
+++ bot/src/main/java/com/jax/bot/api/services/schedule/CheckExternalIp.java
@@ -0,0 +1,23 @@
+package com.jax.bot.api.services.schedule;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class CheckExternalIp {
+//    public static void main(String[] args) {
+//        try {
+//            URL whatIsMyIsIp = new URL("http://checkip.amazonaws.com");
+//            BufferedReader br = null;
+//            br = new BufferedReader(new InputStreamReader(whatIsMyIsIp.openStream()));
+//            String ip = br.readLine();
+//            System.out.println(String.format("My ip is : %s", ip));
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
